3e7e2e5bbd0b1e894d49425544994c30794a93fe
blackchoey <blackchoey@gmail.com>
2022-03-28 15:35:27 +0800

    changes to enable sso

--- bot/src/index.ts
+++ bot/src/index.ts
@@ -2,6 +2,8 @@
 import { TeamsActivityHandler } from "botbuilder";
 import * as restify from "restify";
 import { adapter } from "./internal/initialize";
+import * as path from "path";
+import { TeamsSsoBot } from "./sso/teamsSsoBot";
 
 // Create HTTP server.
 const server = restify.createServer();
@@ -10,9 +12,26 @@ server.listen(process.env.port || process.env.PORT || 3978, () => {
 });
 
 // Process Teams activity with Bot Framework.
-const handler = new TeamsActivityHandler();
+// const handler = new TeamsActivityHandler();
+const handler = new TeamsSsoBot();
+
 server.post("/api/messages", async (req, res) => {
     await adapter.processActivity(req, res, async (context) => {
         await handler.run(context);
+    }).catch((err) => {
+        // Error message including "412" means it is waiting for user's consent, which is a normal process of SSO, sholdn't throw this error.
+        if (!err.message.includes("412")) {
+            throw err;
+        }
     });
 });
+
+
+server.get(
+    "/auth-*.html",
+    restify.plugins.serveStatic({
+        directory: path.join(__dirname, "public"),
+    })
+);
+
+
